a8dbd3c6c52ae9a5e82e075deaee708e90ddfd18
Trevor Millner <trevormillner@gmail.com>
2017-10-23 00:14:48 -0700

    fix - Data generation bug fix

--- generate_data.sh
+++ generate_data.sh
@@ -8,11 +8,17 @@ if [ ! -d "${storage_dir}" ]; then
     mkdir "${storage_dir}"
 fi
 
-dest = "${storage_dir}/${DATE}"
+dest="${storage_dir}/${DATE}"
 
 if [ ! -d "${dest}" ]; then
 	mkdir "${dest}"
 	python run.py "${dest}/"
+
+    if [ "$?" != "0" ]; then
+        echo "Run script did not complete. Exiting"
+        exit 3
+    fi
+
 	aws s3 sync "${dest}" "s3://news.ingestion.project/${DATE}/"
 
 	if [ "$?" = "0" ]; then
--- run.py
+++ run.py
@@ -4,11 +4,12 @@ import urllib2
 import sys
 import os
 
-key = os.environ.get('NEWSAPI_KEY')
-if (key == ""):
+k = os.getenv("NEWSAPI_KEY")
+
+if k == None:
   exit()
 else:
-  url = "https://newsapi.org/v1/articles?apiKey=" + key + "&sortBy=latest&source="
+  url = "https://newsapi.org/v1/articles?apiKey=" + k + "&sortBy=latest&source="
   source=["abc-news-au","al-jazeera-english","ars-technica","associated-press","bbc-news","bbc-sport","bloomberg","breitbart-news","business-insider","business-insider-uk","buzzfeed","cnbc","cnn","daily-mail","engadget","entertainment-weekly","espn","espn-cric-info","financial-times","football-italia","fortune","four-four-two","fox-sports","google-news","hacker-news","ign","independent","mashable","metro","mirror","mtv-news","mtv-news-uk","national-geographic","new-scientist","newsweek","new-york-magazine","nfl-news","polygon","recode","reddit-r-all","reuters","talksport","techcrunch","techradar","the-economist","the-guardian-au","the-guardian-uk","the-hindu","the-huffington-post","the-lad-bible","the-new-york-times","the-next-web","the-sport-bible","the-telegraph","the-times-of-india","the-verge","the-wall-street-journal","the-washington-post","time","usa-today"]
 
   for s in source:
