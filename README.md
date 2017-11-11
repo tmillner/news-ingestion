2747743230a84ed670a786efec961383348509f6
Trevor Millner <trevormillner@gmail.com>
2017-11-10 23:58:45 -0800

    task - Update app w/Angular

--- README.md
+++ README.md
@@ -4,6 +4,5 @@ TODO :
 - Write test to verify db ingestion
 - Refactor with lombok
 - Decouple Webapp BE to own service
-- Fix Spring Icon
 - i18n-ize
 - Logging?
--- src/main/java/news/Application.java
+++ src/main/java/news/Application.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
-@EnableWebMvc
 public class Application {
 
     public static void main(String[] args) {
